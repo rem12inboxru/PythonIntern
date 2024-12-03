bca735942e032c196b03a317e0d2b94939264d2e
rem12inboxru <166938917+rem12inboxru@users.noreply.github.com>
2024-12-03 20:19:16 +0800

    Add files via upload

---
+++ data_download.py
@@ -0,0 +1,17 @@
+import yfinance as yf
+
+
+def fetch_stock_data(ticker, period='1mo'):
+    stock = yf.Ticker(ticker)
+    data = stock.history(period=period)
+    return data
+
+
+def add_moving_average(data, window_size=5):
+    data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
+    return data
+
+
+def calculate_and_display_average_price(data):
+    average_price = data['Close'].mean(axis='index')
+    print(f'Среднее значение колонки "Close" : {average_price}\n')
