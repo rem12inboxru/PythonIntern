12a23470c2e6aa7fdee040f5319f761ab5bb930c
rem12inboxru <166938917+rem12inboxru@users.noreply.github.com>
2024-12-09 00:05:06 +0800

    Add files via upload

--- data_download.py
+++ data_download.py
@@ -15,3 +15,16 @@ def add_moving_average(data, window_size=5):
 def calculate_and_display_average_price(data):
     average_price = data['Close'].mean(axis='index')
     print(f'Среднее значение колонки "Close" : {average_price}\n')
+
+
+def notify_if_strong_fluctuations(data, threshold):
+    threshold = float(input('Введите допустимое значение колебаний цены акций: '))
+    min_price_close = data['Close'].min()
+    max_price_close = data['Close'].max()
+    try:
+        fluctuation = ((max_price_close - min_price_close) / min_price_close) * 100
+        fluctuation1 = round(fluctuation, 2)
+        if fluctuation1 > threshold:
+            print(f'Колебания цены превысили заданное значение на {fluctuation1 - threshold} процентов.')
+    except Exception:
+        print(f'Минимальная цена закрытия равна нулю. Непредвиденная ошибка')
